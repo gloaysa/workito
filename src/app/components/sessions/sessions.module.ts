72b887ff887b4eb4cdb7e37de846a01eb390efbf
Guillermo Loaysa <gloaysa@gmail.com>
2019-06-04 18:02:50 +0200

    create sessions component

---
+++ src/app/components/sessions/sessions.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import {SessionsComponent} from './sessions.component';
+import {SessionDetailsComponent} from './session-details/session-details.component';
+import {DateFnsModule} from 'ngx-date-fns';
+import {SessionsRoutingModule} from './sessions-routing.module';
+
+@NgModule({
+  declarations: [
+    SessionsComponent,
+    SessionDetailsComponent
+  ],
+  imports: [
+    CommonModule,
+    DateFnsModule,
+    SessionsRoutingModule
+  ]
+})
+export class SessionsModule { }
---
+++ src/app/services/services.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+
+import {SessionsService} from './sessions.service';
+
+@NgModule({
+  providers: [
+    SessionsService
+  ]
+})
+export class ServicesModule { }
