18624efe50292dc9bf00b368b9ce451bce298148
Guillermo Loaysa <gloaysa@gmail.com>
2019-06-09 18:09:42 +0200

    move fixed-navbar to elements folder

--- src/app/components/elements/fixed-navbar/fixed-navbar.component.ts
+++ src/app/components/elements/fixed-navbar/fixed-navbar.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {UserService} from '../users/user.service';
+import {UserService} from '../../users/user.service';
 
 @Component({
   selector: 'workito-fixed-navbar',
